034185f5be37db008246df6939c26f7ddae7176d
Egor <koroliov2108@mail.ru>
2023-10-18 17:37:01 +0300

    Это задание 3 там два варианта 10 и 20

---
+++ Z3.1/Z3.1/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Ingredient
+{
+    public string Name { get; set; }
+}
+
+public class Apple : Ingredient
+{
+    public int Quantity { get; set; }
+}
+
+public class Plum : Ingredient
+{
+    public int Quantity { get; set; }
+}
+//обобщенный класс
+public class Compote<T> where T : Ingredient
+{
+    public List<T> Ingredients { get; set; }
+
+    public Compote(List<T> ingredients)
+    {
+        Ingredients = ingredients;
+    }
+
+    public void PrintRecipe()
+    {
+        Console.WriteLine("Ingredients:");
+        foreach (var ingredient in Ingredients)
+        {
+            Console.WriteLine(ingredient.Name);
+        }
+    }
+}
--- Z3/Z3/Program.cs
+++ Z3/Z3/Program.cs
@@ -1,1 +1,83 @@
-﻿
+﻿// Обобщенный класс для компота
+class Compote<T, U>
+{
+    // Список ингредиентов
+    private List<T> ingredientsT;
+    private List<U> ingredientsU;
+
+    public Compote(List<T> tIngredients, List<U> uIngredients)
+    {
+        ingredientsT = tIngredients;
+        ingredientsU = uIngredients;
+    }
+
+    // Метод для вывода рецепта
+    public void PrintRecipe()
+    {
+        Console.WriteLine("Рецепт компота:");
+        Console.WriteLine("Яблоки:");
+        foreach (var item in ingredientsT)
+        {
+            Console.WriteLine($" - {item}");
+        }
+        Console.WriteLine("Сливы:");
+        foreach (var item in ingredientsU)
+        {
+            Console.WriteLine($" - {item}");
+        }
+    }
+}
+
+// Класс для яблок
+class Apple
+{
+    public int Amount { get; set; }
+
+    public Apple(int amount)
+    {
+        Amount = amount;
+    }
+
+    public override string ToString()
+    {
+        return $"Яблоко (количество: {Amount})";
+    }
+}
+
+// Класс для слив
+class Plum
+{
+    public int Amount { get; set; }
+
+    public Plum(int amount)
+    {
+        Amount = amount;
+    }
+
+    public override string ToString()
+    {
+        return $"Слива (количество: {Amount})";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Apple> apples = new List<Apple>
+        {
+            new Apple(3),
+            new Apple(2)
+        };
+
+        List<Plum> plums = new List<Plum>
+        {
+            new Plum(4),
+            new Plum(5)
+        };
+
+        Compote<Apple, Plum> myCompote = new Compote<Apple, Plum>(apples, plums);
+        myCompote.PrintRecipe();
+    }
+}
+
