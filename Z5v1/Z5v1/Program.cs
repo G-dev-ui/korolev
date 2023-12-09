b5c788dbbf5ffd8408e89ceaecce71d688fcf1e4
G-dev-ui <koroliov2108@mail.ru>
2023-12-09 11:43:43 +0300

    Задание 5 вариант 1

---
+++ Z5v1/Z5v1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+public class TooShortStringException : Exception
+{
+    public TooShortStringException(string message) : base(message) { }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.Write("Введите имя файла: ");
+            string filename = Console.ReadLine();
+
+            Console.Write("Введите строку для записи: ");
+            string input = Console.ReadLine();
+
+            if (input.Length < 3)
+            {
+                throw new TooShortStringException("Строка слишком мала");
+            }
+
+            using (StreamWriter sw = new StreamWriter(filename))
+            {
+                sw.Write(input[2]);
+            }
+        }
+        catch (TooShortStringException ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Неизвестная ошибка: " + ex.Message);
+        }
+    }
+}
